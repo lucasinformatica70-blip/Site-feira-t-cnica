326431b979533157927a5d0337066f5c23ca8c16
lucasinformatica70-blip <lucasinformatica70@gmail.com>
2026-08-06 09:30:21 -0300

    começo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta author ="Lucas Vieira de Freitas, Davi Freire da Cunha Reggiani, ">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Como começar: Visual studio code</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./javascript/script.js" defer></script>
+</head>
+
+<body>
+    <h1>this aint gun be easy</h1>
+</body>
+</html>
